8bc8539cbdc3b3fa689851a099025242aa29ca62
Brad Larson <brad@bannerwave.com>
2024-07-19 10:31:48 -0400

    Updated county schema to add ID for easier alert identifiers

--- src/graphql/types/TCountyProperties.js
+++ src/graphql/types/TCountyProperties.js
@@ -1,65 +1,72 @@
-async function TCountyProperties(countyProperties, fields){
-    // Build return object
-    const countyPropertiesInfo = {
-        STATE: await ( async () => {
-            if ('STATE' in fields){
-                return countyProperties.STATE
-            } else {
-                return null
-            }
-        })(),
-        CWA: await ( async () => {
-            if ('CWA' in fields){
-                return countyProperties.CWA
-            } else {
-                return null
-            }
-        })(),
-        COUNTYNAME: await ( async () => {
-            if ('COUNTYNAME' in fields){
-                return countyProperties.COUNTYNAME
-            } else {
-                return null
-            }
-        })(),
-        FIPS: await ( async () => {
-            if ('FIPS' in fields){
-                return countyProperties.FIPS
-            } else {
-                return null
-            }
-        })(),
-        TIME_ZONE: await ( async () => {
-            if ('TIME_ZONE' in fields){
-                return countyProperties.TIME_ZONE
-            } else {
-                return null
-            }
-        })(),
-        FE_AREA: await ( async () => {
-            if ('FE_AREA' in fields){
-                return countyProperties.FE_AREA
-            } else {
-                return null
-            }
-        })(),
-        LON: await ( async () => {
-            if ('LON' in fields){
-                return countyProperties.LON
-            } else {
-                return null
-            }
-        })(),
-        LAT: await ( async () => {
-            if ('LAT' in fields){
-                return countyProperties.LAT
-            } else {
-                return null
-            }
-        })()
-    }
-    // Return requested data
-    return countyPropertiesInfo
+async function TCountyProperties(countyProperties, fields) {
+  // Build return object
+  const countyPropertiesInfo = {
+    ID: await (async () => {
+      if ("FIPS" in fields) {
+        return countyProperties.FIPS;
+      } else {
+        return null;
+      }
+    })(),
+    STATE: await (async () => {
+      if ("STATE" in fields) {
+        return countyProperties.STATE;
+      } else {
+        return null;
+      }
+    })(),
+    CWA: await (async () => {
+      if ("CWA" in fields) {
+        return countyProperties.CWA;
+      } else {
+        return null;
+      }
+    })(),
+    COUNTYNAME: await (async () => {
+      if ("COUNTYNAME" in fields) {
+        return countyProperties.COUNTYNAME;
+      } else {
+        return null;
+      }
+    })(),
+    FIPS: await (async () => {
+      if ("FIPS" in fields) {
+        return countyProperties.FIPS;
+      } else {
+        return null;
+      }
+    })(),
+    TIME_ZONE: await (async () => {
+      if ("TIME_ZONE" in fields) {
+        return countyProperties.TIME_ZONE;
+      } else {
+        return null;
+      }
+    })(),
+    FE_AREA: await (async () => {
+      if ("FE_AREA" in fields) {
+        return countyProperties.FE_AREA;
+      } else {
+        return null;
+      }
+    })(),
+    LON: await (async () => {
+      if ("LON" in fields) {
+        return countyProperties.LON;
+      } else {
+        return null;
+      }
+    })(),
+    LAT: await (async () => {
+      if ("LAT" in fields) {
+        return countyProperties.LAT;
+      } else {
+        return null;
+      }
+    })(),
+  };
+  // Return requested data
+  return countyPropertiesInfo;
 }
 
-module.exports = TCountyProperties
+module.exports = TCountyProperties;
