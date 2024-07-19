3ecf5eade198bc390988731075d5fde5ce347fa6
Brad Larson <brad@bannerwave.com>
2024-07-19 11:56:20 -0400

    fix Colors bug

--- src/graphql/types/THazardColor.js
+++ src/graphql/types/THazardColor.js
@@ -4,16 +4,16 @@ async function THazardColor({ color_rgb, color_hex }) {
   const G = Number(color_rgb.split(",")[1]);
   const B = Number(color_rgb.split(",")[2]);
   const hazardColor = {
-    rgb: await (async () => {
-      if ("rgb" in fields) {
+    RGB: await (async () => {
+      if ("RGB" in fields) {
         const TRGB = require("./TRGB");
         return await TRGB(R, G, B);
       } else {
         return null;
       }
     })(),
-    hex: await (async () => {
-      if ("hex" in fields) {
+    HEX: await (async () => {
+      if ("HEX" in fields) {
         return color_hex;
       } else {
         return null;
